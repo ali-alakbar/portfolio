a341bd39822e957738b86a0b49dc650c0a97e38e
Ali Akbar <84961269+ali-alakbar@users.noreply.github.com>
2026-09-13 13:11:45 +0300

    Update portfolio download to current CV

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -68,7 +68,7 @@ export const HeroSection = ({
               download
               aria-label="Download CV in PDF format"
               className="w-full sm:w-auto"
-              href="/Ali Akbar cv.pdf"
+              href="/Ali_Alakbar_Banimansor_CV.pdf"
             >
               <Button
                 fullWidth
